670d3921571ed8ee91eaa594083958c9fd8b213b
3sdd <56486103+3sdd@users.noreply.github.com>
2019-10-15 23:35:01 +0900

    はろーわーるど

--- HelloWorld/MainWindow.xaml.cs
+++ HelloWorld/MainWindow.xaml.cs
@@ -24,5 +24,10 @@ namespace HelloWorld
         {
             InitializeComponent();
         }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            TextBox1.Text = "Hello World";
+        }
     }
 }
