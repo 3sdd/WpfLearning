fcfb4b7bbeccd5d224c5e1e8264967bf3dc74754
3sdd <56486103+3sdd@users.noreply.github.com>
2019-10-17 23:42:42 +0900

    新しいWindow表示練習

---
+++ NewWindow/MainWindow.xaml.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace NewWindow
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            UpdateWindowCountLabel();
+        }
+
+        private void Window1Button_Click(object sender, RoutedEventArgs e)
+        {
+            var win1 = new Window1();
+            win1.Owner = this;
+            win1.Closed += (sender, e) => UpdateWindowCountLabel();
+            win1.Show();
+            UpdateWindowCountLabel();
+        }
+
+        Window2 win2 = null;
+        private void Window2Button_Click(object sender, RoutedEventArgs e)
+        {
+            foreach (Window window in OwnedWindows)
+            {
+                if (window is Window2)
+                {
+                    return;
+                }
+            }
+
+            var win2 = new Window2();
+            win2.Closed += (sender, e) => UpdateWindowCountLabel();
+            win2.Owner = this;
+            win2.Show();
+            UpdateWindowCountLabel();
+        }
+
+        void UpdateWindowCountLabel()
+        {
+            WindowCountLabel.Content = OwnedWindows.Count;
+        }
+    }
+}
